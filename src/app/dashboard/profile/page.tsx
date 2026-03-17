9a16bf45e2544170101126d925ac343712a7952f
Juan Carlo Vieri <juan.c.vieri@gmail.com>
2026-03-17 18:44:29 +0800

    Remove year

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -102,7 +102,7 @@ const ProfilePage = () => {
               <h1 className="text-2xl sm:text-3xl font-semibold text-gray-800">
                 {user.username} {isSuperhero && "✨"}
               </h1>
-              <p className="text-gray-500">Year {user.year}</p>
+              {/* <p className="text-gray-500">Year {user.year}</p> */}
               <div className="mt-2 flex flex-wrap gap-2 justify-center sm:justify-start">
                 <span className="px-3 py-1 bg-gray-200 text-gray-700 rounded-full text-sm">
                   Points: {roomBidInfo?.points || "Loading..."}
